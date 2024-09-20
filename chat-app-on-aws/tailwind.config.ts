d14152191cf47088bf825fc436a9a83f5aa61854
malaythakur <malaythakur13@gmail.com>
2024-09-20 15:13:47 +0530

    feat: center container and add padding in tailwind config
    - Updated tailwind.config.ts to center the container
    - Added padding of 2rem to the container

--- chat-app-on-aws/tailwind.config.ts
+++ chat-app-on-aws/tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      container: {
+        center: true,
+        padding: "2re"
+      },
     },
   },
   plugins: [],
