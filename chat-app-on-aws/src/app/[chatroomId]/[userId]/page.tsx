710da2fb59682669426cd0f88de2576c9499043a
malaythakur <malaythakur13@gmail.com>
2024-09-28 10:28:45 +0530

    feat: integrate GraphQL for chat functionality
    - Added real-time chat fetching with 'chatsByChatroom' query
    - Implemented subscription to new messages with 'onCreateChat'
    - Added message sending functionality via 'createChat' mutation
    - Updated ChatComp to display fetched and live chats

--- chat-app-on-aws/src/app/[chatroomId]/[userId]/page.tsx
+++ chat-app-on-aws/src/app/[chatroomId]/[userId]/page.tsx
@@ -1,4 +1,11 @@
+'use client';
+import { Chat } from "@/API";
 import ChatComp from "@/components/chat";
+import { createChat } from "@/graphql/mutations";
+import { chatsByChatroom } from "@/graphql/queries";
+import { onCreateChat } from "@/graphql/subscriptions";
+import { client } from "@/lib/client";
+import { useEffect, useState } from "react";
 
 interface PageProps {
     params: {
@@ -7,12 +14,72 @@ interface PageProps {
     };
 }
 
-
-
 export default function Page({ params }: PageProps) {
+
+    const [chats, setChats] = useState<Chat[]>([]);
+
+    useEffect(() => {
+
+        client
+            .graphql({
+                query: chatsByChatroom,
+                variables: {
+                    chatroomId: params.chatroomId,
+                },
+            }).then((res) => {
+                setChats(res.data.chatsByChatroom.items)
+            });
+    }, [params.chatroomId]);
+
+
+    useEffect(() => {
+        const subscription = client
+            .graphql({
+                query: onCreateChat,
+                variables: {
+                    filter: {
+                        chatroomId: {
+                            eq: params.chatroomId,
+                        },
+                    },
+                },
+            }).subscribe({
+                next(value) {
+                 
+
+                setChats((chats) => [...chats, value.data.onCreateChat]);
+                },
+
+            });
+
+        return ()=> {
+            subscription.unsubscribe();
+        }
+    }, [params.chatroomId])
+
+    async function handleSend(text: string) {
+        await client.graphql({
+            query: createChat,
+            variables: {
+                input: {
+                    chatroomId: params.chatroomId,
+                    sender: params.userId,
+                    text
+                }
+            }
+        })
+    }
     return (
         <div className="container max-w-screen-sm mt-20">
-            <ChatComp />
-        </div>
+            <ChatComp chats={chats.map(chat => {
+                if (!chat) return null; // Check for null
+                return {
+                    ...chat,
+                    incoming: chat.sender !== params.userId,
+                };
+            }).filter(chat => chat !== null)} 
+            onSend={handleSend}
+            />
+       </div>
     );
 }
