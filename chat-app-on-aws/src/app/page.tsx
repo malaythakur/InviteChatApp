524ea03c65089d0cec2df5129e78f675667a10d4
malaythakur <malaythakur13@gmail.com>
2024-09-28 10:37:40 +0530

    feat: add chatroom creation functionality
    - Integrated input for username to create a chatroom
    - Implemented chatroom creation using 'createChat' mutation
    - Navigated to the new chatroom page upon creation
    - Enhanced UI layout for chatroom creation

--- chat-app-on-aws/src/app/page.tsx
+++ chat-app-on-aws/src/app/page.tsx
@@ -1,6 +1,46 @@
-import ChatComp from "@/components/chat";
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { client } from "@/lib/client";
+import {v4} from 'uuid';
+import { createChat } from "@/graphql/mutations";
+
 
 export default function Home() {
-  return <ChatComp/>
-    
+  const [username, setUsername] = useState(""); 
+  const router = useRouter()
+
+  function handleCreate() {
+    if (username.trim() === "") return;
+
+    const chatroomId = v4()
+
+    client.graphql({
+      query: createChat,
+      variables: {
+        input: {
+          chatroomId,
+          sender: username,
+          text: `${username} has created this chatroom, chatroom link: ${window.location.origin}/${chatroomId}`
+        }
+      }
+    }).then(()=>{
+      router.push(`${chatroomId}/${username}`)
+    })
+
+  }
+
+
+  return (
+    <div className="container max-w-screen-sm mt-20">
+      <h1 className="text-2xl">Create Chatroom</h1>
+      <div className="mt-4 space-y-2">
+        <Input placeholder="Enter your name" value={username} onChange={e => setUsername(e.target.value)}></Input>
+        <Button onClick={handleCreate}>Create</Button>
+      </div>
+    </div>
+  );
 }
