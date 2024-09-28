3af71a3bbf08185a418d1d3bbf4d0bed43c25fad
malaythakur <malaythakur13@gmail.com>
2024-09-28 10:42:55 +0530

    refactor: update ChatComp to accept dynamic chat messages
    - Defined Chat and Props interfaces for better type safety
    - Replaced static messages with dynamic rendering of chat messages
    - Utilized 'chats' prop to map through messages
    - Updated Message component to reflect incoming and outgoing messages
    - Enhanced MessageInput to handle message sending via onSend prop

--- chat-app-on-aws/src/components/chat.tsx
+++ chat-app-on-aws/src/components/chat.tsx
@@ -2,37 +2,45 @@
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
 
-export default function ChatComp() {
+interface Chat {
+    id: string;
+    sender: string;
+    text: string;
+    incoming? : boolean;
+}
+
+interface Props {
+    chats: Chat[],
+    onSend: (text: string) => any
+}
+
+export default function ChatComp({chats, onSend} : Props) {
     return (
         <div style={{ position: "relative", height: "500px" }}>
             <MainContainer>
                 <ChatContainer>
                     <MessageList>
-                        <Message model={{
-                            direction: "incoming",
-                            position: "first",
-                            message: "Hello my friend",
-                            sentTime: "just now",
-                            sender: "Joe"
-                        }} />
+                        {
+                            chats.map((chat) => (
+                                <Message 
+                                    key={chat.id}
+                                    model={{
+                                    direction: chat.incoming ? 'incoming' : 'outgoing',
+                                    position: "single",
+                                    message: chat.text,  
+                                    sender: chat.sender,
+                                }} 
+                                >
+                                    <Message.Header sender={chat.sender} />
+                                </Message>
+                                
+                            ))
+                        }
+                       
 
-                        <Message model={{
-                            direction: "incoming",
-                            position: "last",
-                            message: "Hello my friend 2",
-                            sentTime: "just now",
-                            sender: "Joe"
-                        }} />
-
-                        <Message model={{
-                            direction: "outgoing",
-                            position: "single",
-                            message: "Hi there",
-                            sentTime: "just now",
-                            sender: "Joe"
-                        }} />
+                    
                     </MessageList>
-                    <MessageInput placeholder="Type message here" />
+                    <MessageInput placeholder="Type message here" attachButton={false} onSend={onSend}/>
                 </ChatContainer>
             </MainContainer>
         </div>
